d063073fad5a5595d79a9c4147dbcef3529a1dbe
Falko Basner <falko.basner@codecentric.de>
2020-09-20 06:30:36 +0200

    remove unused code

--- src/main/java/de/demo/Application.java
+++ src/main/java/de/demo/Application.java
@@ -5,8 +5,6 @@ import de.demo.user.UserApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -14,10 +12,5 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
